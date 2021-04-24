91e210281e807eaec808f282684544796e12b7a3
Jahonay <xjmackey@gmail.com>
2021-04-24 08:24:38 -0400

    Add files via upload

--- src/MyWork.js
+++ src/MyWork.js
@@ -16,7 +16,7 @@ function MyWork() {
                     <div className='three-coll-cont'>
                         <div className='site1' 
                        >
-                            <a href='gregorymsullivanlaw.org'>gregorymsullivanlaw.org</a>
+                            <a target="_blank" rel="noreferrer" href='https://www.gregorymsullivanlaw.org'>gregorymsullivanlaw.org</a>
                             <p>This is a website I made for a family friend
                                 I wanted it to look professional and be functional. 
                             </p>
@@ -26,7 +26,7 @@ function MyWork() {
 
                         <div className='site2'
                         >
-                            <a href='johnmackeydesigns.com'>johnmackeydesigns.com</a>
+                            <a target='_blank' rel="noreferrer" href='https://www.johnmackeydesigns.com'>johnmackeydesigns.com</a>
                             <p>This is a portfolio website I made for myself. I like
                                 some of my choices but I would design it differently 
                                 if I were to remake it today. 
@@ -36,7 +36,7 @@ function MyWork() {
 
                         <div className='site3'
                        >
-                             <a href='globalchildrenschool.com'>globalchildrenschool.com</a>
+                             <a target='_blank'rel="noreferrer" href='https://www.globalchildrenschool.com'>globalchildrenschool.com</a>
                             <p>This is the website redesign that I am 
                                 working on for my current internship. It still needs
                                 some copy to fill out the page before it can go live.
