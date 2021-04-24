a380e4bb94b5160d472a38b82b416096be80aab8
Jahonay <xjmackey@gmail.com>
2021-04-24 16:40:00 -0400

    Add files via upload

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import './HeroSection.css';
+import {Button} from 'reactstrap';
+
+
+
 
 function HeroSection(){
+    
     return(
         <div className='hero-container'>
                 <h1>Are you looking for a website designer?</h1>
-                <p>Let's connect</p>
+                <Button outline color="dark" size='lg'>Lets Connect</Button>
         </div>
 
     );
--- src/component/Cont.js
+++ src/component/Cont.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-import Contact from '../Contact.js';
+import Contact from '../Contact';
 
 export default function Cont() {
     return (
