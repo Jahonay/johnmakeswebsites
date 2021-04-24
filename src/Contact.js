c1f2e8aa4cd05bc241a815381e58c15bf44de9c3
Jahonay <xjmackey@gmail.com>
2021-04-24 09:49:28 -0400

    Add files via upload

--- src/Contact.js
+++ src/Contact.js
@@ -13,24 +13,27 @@ function Contact() {
                     <h2>Contact</h2>
 
                     <div className='form-wrap'>
-                    <form id='contact' name="contact" method="POST" data-netlify="true">
+                    <form action="POST" data-netlify="true">
                    
                         <label>
                             Name:
-                            <input type="text" required="true" id="name"></input>
+                            <input type="text" required="true" name="name" id="name"></input>
                             </label>
                         <label>
                             Phone Number:
-                            <input type="tel" id="phone"></input>
+                            <input type="tel" id="phone" name="phone"></input>
                         </label>                        
                         <label>
                             Email:
-                            <input type="email" required="true" id="email"></input>
+                            <input type="email" required="true" name="email" id="email"></input>
                         </label>
                         <label>
                             <h5>Message:</h5>
-                            <input type="text" required="true" id="message"></input>
+                            <input type="text" required="true" name="message" id="message"></input>
                         </label>
+                            <div className='recaptcha'>
+                                <div data-netlify-recaptcha="true"></div>
+                            </div>
                         <label>
                             <input type="submit" value='submit'></input>
                         </label>
